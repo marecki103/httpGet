a1c07477cf4e5cc01c2f74d9cc81f61a5141874f
My <you@example.com>
2021-03-11 19:15:47 +0000

    Interface Reader,Writer

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -12,6 +15,14 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+	lw := logWriter{}
 
-	fmt.Println(resp)
+	//writer interface, reader interface
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
